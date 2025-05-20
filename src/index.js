3873631fb2da41b9f731a907133f3e4a2aa659b3
Yoga Krisna utama <yogacode862gmail.com>
2025-05-20 15:51:39 +0700

    codingan baru

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.js"; // tambahkan .js di akhir
 import reportWebVitals from "./reportWebVitals.js"; // tambahkan .js di akhir
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
